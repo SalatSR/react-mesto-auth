56cbad90751d6b4b14baa50fd33795f40510b54f
Stas Razborskii <stasaktivniy@yandex.ru>
2022-08-07 22:03:47 +0300

    Fix header link second time

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
 
   return (
     <header className="header">
-      <a href="https://salatsr.github.io/mesto-react-auth/"
+      <a href="https://salatsr.github.io/react-mesto-auth/"
         className="header__logo-link">
         <img src={logo}
           alt="Логотип Mestro Russia"
